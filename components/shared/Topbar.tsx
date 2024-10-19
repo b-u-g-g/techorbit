b8bc54b177b1aa764bef795fe7fa2dd30bf7ed48
b-u-g-g <tsakshi1708@gmail.com>
2024-10-20 03:03:33 +0530

    notes feature

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -7,7 +7,7 @@ function Topbar() {
   return (
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
-        <Image src='/assets/logo.jpeg' alt='logo' width={39} height={39} />
+        <Image src='/assets/logo.jpeg' alt='logo' width={46} height={46} />
         <p className='text-heading3-bold text-light-1 max-xs:hidden'>ByteSpace</p>
       </Link>
 
